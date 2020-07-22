52823e5a1182938c0c045d798b09140bfe27423b
Ludovic G <dev@ludovicgeneix.fr>
2020-07-22 19:05:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Dofus avec wineprefix
+
+Cette version de Wine est créée par Lutris et inclue les patchs proton de Valve ainsi que d'autres fix créés par la communauté.
+
+Ce script corrige :
+- le problème du jeu bloqué à 56% au lancement
+- le plantage de wineserver de temps en temps (jeu figé)
+- jeu plus fluide
+
+NB: Le script sera peu être à relancer après les MAJ.
+
+### Prérequis
+- winetricks (souvent déjà installé)
+
+**Debian / Ubuntu**
+```$ sudo apt-get install winetricks```
+
+**Arch / Manjaro**
+```$ sudo pacman -S winetricks```
+
+### Utilisation
+- copier le script dans le répertoire du jeu, par défaut : ~/.config/Ankama/zaap/dofus (ctrl+h pour voir les fichiers cachés)
+- ouvrir un terminal et lancer ./configure-wine.sh
+- patienter 
+- lancer le jeu via Ankama Launcher :)
---
+++ configure-wine.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+currentpath="$PWD"
+winever="5.7-8"
+
+# download lutris wine build
+if [ ! -d "lutris-$winever-x86_64" ]; then
+    wget https://github.com/lutris/wine/releases/download/lutris-$winever/wine-lutris-$winever-x86_64.tar.xz
+    tar -xf wine-lutris-$winever-x86_64.tar.xz
+    rm wine-lutris-$winever-x86_64.tar.xz
+fi
+
+# create wine environment
+if [ ! -d ".wine" ]; then
+    mkdir .wine
+fi
+
+# backup current script
+if [ ! -f "zaap-start.old" ]; then
+    mv zaap-start.sh zaap-start.old
+else
+    rm zaap-start.sh
+fi
+
+# create new script
+touch zaap-start.sh
+echo $currentpath >> zaap-start.sh
+echo "#!/bin/sh" >> zaap-start.sh
+echo "WINEPREFIX=$currentpath/.wine $currentpath/lutris-$winever-x86_64/bin/wine Dofus.exe --port=\$ZAAP_PORT --gameName=\$ZAAP_GAME --gameRelease=\$ZAAP_RELEASE --instanceId=\$ZAAP_INSTANCE_ID --hash=\$ZAAP_HASH --canLogin=\$ZAAP_CAN_AUTH > /dev/null 2>&1" >> zaap-start.sh
+echo "exit \$?" >> zaap-start.sh
+
+# add execute to script
+chmod +x zaap-start.sh

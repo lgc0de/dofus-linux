cea0ec74a1aa386ce40f76a9bebf80e3aeb7662d
Ludovic G <dev@ludovicgeneix.fr>
2022-10-19 23:20:09 +0200

    update wine to 7.2-2

--- dofus-linux.sh
+++ dofus-linux.sh
@@ -3,7 +3,7 @@ dir="$PWD"
 lutriswinepath="/home/$USER/.local/share/lutris/runners/wine"
 
 # change version number to downgrade to another old build
-winever="7.2"
+winever="7.2-2"
 
 # fsync support (only work if you have a kernel with futex sync support)
 # put 0 if you want to disable fsync ()
